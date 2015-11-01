f6734deadb5334e5ee0b8481754e949c40032cd1
unknown <tim.deweert@ugent.be>
2015-11-01 11:05:26 +0100

    Split shapepredictor function

--- predict/shapepredictor.py
+++ predict/shapepredictor.py
@@ -1,4 +1,3 @@
-
 import os
 import cv2
 from PIL import Image
@@ -20,68 +19,74 @@ __author__ = 'Group16'
 
 """
 
-class ShapePredictor():
 
+class ShapePredictor():
     @staticmethod
     def predictShape(hue):
-            img = (np.rint(asarray(hue)*255)).astype(np.uint8)
-            img = resize(img,(64,64))
-            ret, thresh = cv2.threshold(img, 127, 255,0)
-            imgg, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,1)
-            if len(contours) == 0:
-                return [0,0,0,0]
-            index = 0;
-            max_area = cv2.contourArea(contours[0])
-            for i in range(0,len(contours)):
-                area = cv2.contourArea(contours[i])
-                if area > max_area:
-                    max_area = area
-                    index = i
-            contour = contours[index]
+        img = (np.rint(asarray(hue) * 255)).astype(np.uint8)
+        img = resize(img, (64, 64))
+        ret, thresh = cv2.threshold(img, 127, 255, 0)
+        imgg, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, 1)
+        if len(contours) == 0:
+            return None
+        index = 0;
+        max_area = cv2.contourArea(contours[0])
+        for i in range(0, len(contours)):
+            area = cv2.contourArea(contours[i])
+            if area > max_area:
+                max_area = area
+                index = i
+        return contours[index]
 
-            desc = ZernikeMoments(21)
-            moments = desc.describe(contour)
-            return moments
-            
-            """
-            mu = cv2.moments(contour,False)
+    @staticmethod
+    def calculateGeometricMoments(contour):
+        if contour == None:
+            return [0, 0, 0, 0]
 
-            #The zero order central moment is zero so no shape can be predicted
-            if(mu["m00"] == 0):
-                return [0,0,0,0]
+        mu = cv2.moments(contour, False)
 
-            #Calculate first moment invariant
-            I = (mu["mu20"]*mu["mu02"] - mu["mu11"]**2)/(mu["m00"]**4)
+        # The zero order central moment is zero so no shape can be predicted
+        if (mu["m00"] == 0):
+            return [0, 0, 0, 0]
 
-            #Measure ellipticity
-            E = 0
-            if I < 1/(16*math.pi**2):
-                E = 16*(math.pi**2)*I
-            else:
-                E =  1/(16*(math.pi**2)*I)
+        # Calculate first moment invariant
+        I = (mu["mu20"] * mu["mu02"] - mu["mu11"] ** 2) / (mu["m00"] ** 4)
 
-            #Measure triangularity
-            T = 0
-            if I < 1/108:
-                T = 108*I
-            else:
-                T =  1/(108*I)
+        # Measure ellipticity
+        E = 0
+        if I < 1 / (16 * math.pi ** 2):
+            E = 16 * (math.pi ** 2) * I
+        else:
+            E = 1 / (16 * (math.pi ** 2) * I)
 
-            #Measure octagonality
-            O = 0
-            if I < 1/(15.932*math.pi**2):
-                O = 15.932*(math.pi**2)*I
-            else:
-                O =  1/(15.932*(math.pi**2)*I)
+        # Measure triangularity
+        T = 0
+        if I < 1 / 108:
+            T = 108 * I
+        else:
+            T = 1 / (108 * I)
 
-            #Measure Rectangularity
-            minRect = cv2.minAreaRect(contour)
-            box = cv2.boxPoints(minRect)
-            area = cv2.contourArea(contour)
-            areaMinRect = cv2.contourArea(box)
-            R = area / areaMinRect
+        # Measure octagonality
+        O = 0
+        if I < 1 / (15.932 * math.pi ** 2):
+            O = 15.932 * (math.pi ** 2) * I
+        else:
+            O = 1 / (15.932 * (math.pi ** 2) * I)
 
-            return[E,T,O,R]
-            """
+        # Measure Rectangularity
+        minRect = cv2.minAreaRect(contour)
+        box = cv2.boxPoints(minRect)
+        area = cv2.contourArea(contour)
+        areaMinRect = cv2.contourArea(box)
+        R = area / areaMinRect
 
-#3.4109452905384194
+        return [E, T, O, R]
+
+    @staticmethod
+    def calculateZernikMoments(contour):
+        return []
+
+
+
+# 3.4109452905384194
+

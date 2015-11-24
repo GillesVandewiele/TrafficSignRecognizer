adc41eacfcbba38abb0844ec36c9d2e00289490a
unknown <tim.deweert@ugent.be>
2015-11-24 16:34:55 +0100

    la

--- cnn.py
+++ cnn.py
@@ -59,7 +59,7 @@ def preprocess_image(image):
     image_array = cv2.imread(image)
     #denoised_image = cv2.fastNlMeansDenoisingColored(image_array,None,7,7,11,21)
     gray_image = color.rgb2gray(image_array)
-    resized_image = resize(gray_image, (64, 64))
+    resized_image = resize(gray_image, (32, 32))
     return expand_dims(resized_image, axis=0)
 
 
@@ -144,7 +144,7 @@ def build_cnn(input_var=None):
     # and a fully-connected hidden layer in front of the output layer.
 
     # Input layer, as usual:
-    network = lasagne.layers.InputLayer(shape=(None, 1, 64, 64),
+    network = lasagne.layers.InputLayer(shape=(None, 1, 32, 32),
                                         input_var=input_var)
     # This time we do not apply input dropout, as it tends to work less well
     # for convolutional layers.
@@ -152,16 +152,31 @@ def build_cnn(input_var=None):
     # Convolutional layer with 32 kernels of size 5x5. Strided and padded
     # convolutions are supported as well; see the docstring.
     network = lasagne.layers.Conv2DLayer(
-            network, num_filters=32, filter_size=(8, 8),
+            network, num_filters=32, filter_size=(3, 3), stride=1, pad='same',
             nonlinearity=lasagne.nonlinearities.rectify,
             W=lasagne.init.GlorotUniform())
-    # Expert note: Lasagne provides alternative convolutional layers that
-    # override Theano's choice of which implementation to use; for details
-    # please see http://lasagne.readthedocs.org/en/latest/user/tutorial.html.
 
     # Max-pooling layer of factor 2 in both dimensions:
-    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
+    network = lasagne.layers.MaxPool2DLayer(network, stride=2, pool_size=(2, 2))
 
+    network = lasagne.layers.Conv2DLayer(
+            network, num_filters=64, filter_size=(4, 4), stride=1, pad='same',
+            nonlinearity=lasagne.nonlinearities.rectify,
+            W=lasagne.init.GlorotUniform())
+
+    # Max-pooling layer of factor 2 in both dimensions:
+    network = lasagne.layers.MaxPool2DLayer(network, stride=2, pool_size=(2, 2))
+
+    """
+    network = lasagne.layers.Conv2DLayer(
+            network, num_filters=128, filter_size=(3, 3), stride=1, pad='same',
+            nonlinearity=lasagne.nonlinearities.rectify,
+            W=lasagne.init.GlorotUniform())
+
+    # Max-pooling layer of factor 2 in both dimensions:
+    network = lasagne.layers.MaxPool2DLayer(network, stride=2, pool_size=(2, 2))
+    """
+    """
     # Another convolution with 32 5x5 kernels, and another 2x2 pooling:
     network = lasagne.layers.Conv2DLayer(
             network, num_filters=32, filter_size=(8, 8),
@@ -173,10 +188,23 @@ def build_cnn(input_var=None):
             lasagne.layers.dropout(network, p=.5),
             num_units=256,
             nonlinearity=lasagne.nonlinearities.rectify)
-
+    """
+    """
     # And, finally, the 10-unit output layer with 50% dropout on its inputs:
     network = lasagne.layers.DenseLayer(
             lasagne.layers.dropout(network, p=.5),
+            num_units=81,
+            nonlinearity=lasagne.nonlinearities.softmax)
+    """
+
+
+    network = lasagne.layers.Conv2DLayer(
+            network, num_filters=128, filter_size=(4, 4), stride=1, pad='same',
+            nonlinearity=lasagne.nonlinearities.rectify,
+            W=lasagne.init.GlorotUniform())
+
+    network = lasagne.layers.DenseLayer(
+            network,
             num_units=81,
             nonlinearity=lasagne.nonlinearities.softmax)
 
@@ -193,10 +221,6 @@ def build_cnn(input_var=None):
 # several changes in the main program, though, and is not demonstrated here.
 
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
-    print(type(inputs))
-    print(type(targets))
-    print(inputs.shape)
-    print(targets.shape)
     assert len(inputs) == len(targets)
     if shuffle:
         indices = np.arange(len(inputs))
@@ -214,137 +238,144 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
 # more functions to better separate the code, but it wouldn't make it any
 # easier to read.
 
-def main(model='cnn', num_epochs=500):
+def main(model='cnn', num_epochs=10):
     # Load the dataset
     print("Loading data...")
     # X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
 
     train_images_dir = os.path.join(os.path.dirname(__file__), "train")
+    #TODO: gelabelde test images gebruiken
     train_images = get_images_from_directory(train_images_dir)
     all_train_images = []
     for image in train_images:
         all_train_images.append(preprocess_image(image))
-    X_train = np.asarray(all_train_images)
     results = get_results(train_images_dir)
     all_results = []
     for result in results:
         all_results.append(result)
-    y_train = np.asarray(all_results)
+    kf = KFold(len(all_train_images), n_folds=2, shuffle=True, random_state=1337)
+    for train, validation in kf:
+
+        X_train = np.asarray([all_train_images[i%len(all_train_images)] for i in train])
+        X_val = np.asarray([all_train_images[i%len(all_train_images)] for i in validation])
+        y_train = np.asarray([all_results[i%len(all_train_images)] for i in train])
+        y_val = np.asarray([all_results[i%len(all_train_images)] for i in validation])
+
+        # print(type(X_train))
+        # print(type(X_train[0]))
+        # print(X_train[0].shape)
+        # print(X_train[0])
+        # print(type(y_train))
+        # print(y_train.shape)
+        # print(y_train)
 
 
-    # print(type(X_train))
-    # print(type(X_train[0]))
-    # print(X_train[0].shape)
-    # print(X_train[0])
-    # print(type(y_train))
-    # print(y_train.shape)
-    # print(y_train)
+        # Prepare Theano variables for inputs and targets
+        input_var = T.tensor4('inputs')
+        target_var = T.ivector('targets')
 
+        # Create neural network model (depending on first command line parameter)
+        print("Building model and compiling functions...")
+        if model == 'mlp':
+            network = build_mlp(input_var)
+        elif model.startswith('custom_mlp:'):
+            depth, width, drop_in, drop_hid = model.split(':', 1)[1].split(',')
+            network = build_custom_mlp(input_var, int(depth), int(width),
+                                       float(drop_in), float(drop_hid))
+        elif model == 'cnn':
+            network = build_cnn(input_var)
+        else:
+            print("Unrecognized model type %r." % model)
+            return
 
-    # Prepare Theano variables for inputs and targets
-    input_var = T.tensor4('inputs')
-    target_var = T.ivector('targets')
+        # Create a loss expression for training, i.e., a scalar objective we want
+        # to minimize (for our multi-class problem, it is the cross-entropy loss):
+        prediction = lasagne.layers.get_output(network)
+        loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+        loss = loss.mean()
+        # We could add some weight decay as well here, see lasagne.regularization.
 
-    # Create neural network model (depending on first command line parameter)
-    print("Building model and compiling functions...")
-    if model == 'mlp':
-        network = build_mlp(input_var)
-    elif model.startswith('custom_mlp:'):
-        depth, width, drop_in, drop_hid = model.split(':', 1)[1].split(',')
-        network = build_custom_mlp(input_var, int(depth), int(width),
-                                   float(drop_in), float(drop_hid))
-    elif model == 'cnn':
-        network = build_cnn(input_var)
-    else:
-        print("Unrecognized model type %r." % model)
-        return
+        # Create update expressions for training, i.e., how to modify the
+        # parameters at each training step. Here, we'll use Stochastic Gradient
+        # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
+        params = lasagne.layers.get_all_params(network, trainable=True)
+        updates = lasagne.updates.nesterov_momentum(
+                loss, params, learning_rate=0.01, momentum=0.9)
 
-    # Create a loss expression for training, i.e., a scalar objective we want
-    # to minimize (for our multi-class problem, it is the cross-entropy loss):
-    prediction = lasagne.layers.get_output(network)
-    loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
-    loss = loss.mean()
-    # We could add some weight decay as well here, see lasagne.regularization.
+        # Create a loss expression for validation/testing. The crucial difference
+        # here is that we do a deterministic forward pass through the network,
+        # disabling dropout layers.
+        test_prediction = lasagne.layers.get_output(network, deterministic=True)
+        test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
+                                                                target_var)
+        test_loss = test_loss.mean()
+        # As a bonus, also create an expression for the classification accuracy:
+        test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
+                          dtype=theano.config.floatX)
 
-    # Create update expressions for training, i.e., how to modify the
-    # parameters at each training step. Here, we'll use Stochastic Gradient
-    # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
-    params = lasagne.layers.get_all_params(network, trainable=True)
-    updates = lasagne.updates.nesterov_momentum(
-            loss, params, learning_rate=0.01, momentum=0.9)
+        # Compile a function performing a training step on a mini-batch (by giving
+        # the updates dictionary) and returning the corresponding training loss:
+        train_fn = theano.function([input_var, target_var], loss, updates=updates)
 
-    # Create a loss expression for validation/testing. The crucial difference
-    # here is that we do a deterministic forward pass through the network,
-    # disabling dropout layers.
-    test_prediction = lasagne.layers.get_output(network, deterministic=True)
-    test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
-                                                            target_var)
-    test_loss = test_loss.mean()
-    # As a bonus, also create an expression for the classification accuracy:
-    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
-                      dtype=theano.config.floatX)
+        # Compile a second function computing the validation loss and accuracy:
+        val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
 
-    # Compile a function performing a training step on a mini-batch (by giving
-    # the updates dictionary) and returning the corresponding training loss:
-    train_fn = theano.function([input_var, target_var], loss, updates=updates)
+        # Finally, launch the training loop.
+        print("Starting training...")
+        # We iterate over epochs:
+        for epoch in range(num_epochs):
+            # In each epoch, we do a full pass over the training data:
+            train_err = 0
+            train_batches = 0
+            start_time = time.time()
+            for batch in iterate_minibatches(X_train, y_train, 100, shuffle=True):
+                inputs, targets = batch
+                train_err += train_fn(inputs, targets)
+                train_batches += 1
 
-    # Compile a second function computing the validation loss and accuracy:
-    val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
+            print("     Start val training")
 
-    # Finally, launch the training loop.
-    print("Starting training...")
-    # We iterate over epochs:
-    for epoch in range(num_epochs):
-        # In each epoch, we do a full pass over the training data:
-        train_err = 0
-        train_batches = 0
-        start_time = time.time()
-        for batch in iterate_minibatches(X_train, y_train, 500, shuffle=True):
-            inputs, targets = batch
-            train_err += train_fn(inputs, targets)
-            train_batches += 1
+            # # And a full pass over the validation data:
+            val_err = 0
+            val_acc = 0
+            val_batches = 0
+            for batch in iterate_minibatches(X_val, y_val, 100, shuffle=True):
+                inputs, targets = batch
+                err, acc = val_fn(inputs, targets)
+                val_err += err
+                val_acc += acc
+                val_batches += 1
 
-        # # And a full pass over the validation data:
-        # val_err = 0
-        # val_acc = 0
-        # val_batches = 0
-        # for batch in iterate_minibatches(X_val, y_val, 500, shuffle=False):
+            # Then we print the results for this epoch:
+            print("Epoch {} of {} took {:.3f}s".format(
+                epoch + 1, num_epochs, time.time() - start_time))
+            print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
+            print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
+            print("  validation accuracy:\t\t{:.2f} %".format(
+                val_acc / val_batches * 100))
+
+        # After training, we compute and print the test error:
+        test_err = 0
+        test_acc = 0
+        test_batches = 0
+        # for batch in iterate_minibatches(X_test, y_test, 500, shuffle=False):
         #     inputs, targets = batch
         #     err, acc = val_fn(inputs, targets)
-        #     val_err += err
-        #     val_acc += acc
-        #     val_batches += 1
+        #     test_err += err
+        #     test_acc += acc
+        #     test_batches += 1
+        print("Final results:")
+        print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
+        print("  test accuracy:\t\t{:.2f} %".format(
+            test_acc / test_batches * 100))
 
-        # Then we print the results for this epoch:
-        print("Epoch {} of {} took {:.3f}s".format(
-            epoch + 1, num_epochs, time.time() - start_time))
-        print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
-        # print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
-        # print("  validation accuracy:\t\t{:.2f} %".format(
-        #     val_acc / val_batches * 100))
-
-    # After training, we compute and print the test error:
-    test_err = 0
-    test_acc = 0
-    test_batches = 0
-    # for batch in iterate_minibatches(X_test, y_test, 500, shuffle=False):
-    #     inputs, targets = batch
-    #     err, acc = val_fn(inputs, targets)
-    #     test_err += err
-    #     test_acc += acc
-    #     test_batches += 1
-    print("Final results:")
-    print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
-    print("  test accuracy:\t\t{:.2f} %".format(
-        test_acc / test_batches * 100))
-
-    # Optionally, you could now dump the network weights to a file like this:
-    # np.savez('model.npz', *lasagne.layers.get_all_param_values(network))
-    #
-    # And load them again later on like this:
-    # with np.load('model.npz') as f:
-    #     param_values = [f['arr_%d' % i] for i in range(len(f.files))]
-    # lasagne.layers.set_all_param_values(network, param_values)
+        # Optionally, you could now dump the network weights to a file like this:
+        # np.savez('model.npz', *lasagne.layers.get_all_param_values(network))
+        #
+        # And load them again later on like this:
+        # with np.load('model.npz') as f:
+        #     param_values = [f['arr_%d' % i] for i in range(len(f.files))]
+        # lasagne.layers.set_all_param_values(network, param_values)
 
 
 if __name__ == '__main__':

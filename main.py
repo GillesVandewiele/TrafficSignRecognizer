b7519af8b81328443c030dd5c3abe115fbb84988
Karsten Goossens <Karsten.Goossens@UGent.be>
2015-12-02 19:09:59 +0100

    data augmentation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+import random
+import shutil
+import string
+from skimage import io, transform
 from predict.colorfeatureextractor import ColorFeatureExtractor
 from predict.hogfeatureextractor import HogFeatureExtractor
 from predict.shapefeatureextractor import ShapeFeatureExtractor
@@ -13,17 +17,34 @@ __author__ = 'Group16'
     Written by Group 16: Tim Deweert, Karsten Goossens & Gilles Vandewiele
     Commissioned by UGent, course Machine Learning
 """
-def get_images_from_directory(directory):
-    # Get all files from a directory
-    images = []
-    for root, subFolders, files in os.walk(directory):
-        for file in files:
-            images.append(os.path.join(root, file))
+def random_augmentation(image):
+    #return transform.warp(image, transform.AffineTransform(scale=[random.uniform(0.8, 1.2)]*2, rotation=random.uniform(-0.1, 0.1)))
 
-    return images
+    type = ['rotate', 'rescale', 'shear'][random.randint(0, 2)]
+    if type == 'rotate':
+        return transform.rotate(image, random.uniform(-5, 5))
+    if type == 'rescale':
+        return transform.rescale(image, random.uniform(0.5, 1.5))
+    if type == 'shear':
+        return transform.warp(image, transform.AffineTransform(shear=random.uniform(-0.1, 0.1)))
+
+    return image
+
+print("Copying training data to new folder ...")
+shutil.rmtree(os.path.join(os.path.dirname(__file__), "train_augmented"))
+shutil.copytree(os.path.join(os.path.dirname(__file__), "train"), os.path.join(os.path.dirname(__file__), "train_augmented"))
+
+print("Augmenting training data ...")
+for root, subFolders, files in os.walk(os.path.join(os.path.dirname(__file__), "train_augmented")):
+    for file in files:
+        image = io.imread(os.path.join(root, file))
+        for i in range(1):
+            name = os.path.join(root, file[:-4] + string.ascii_lowercase[i] + ".png")
+            print("New data:", name)
+            io.imsave(name, random_augmentation(image))
 
 ### VARIABLES ###
-train_images_dir = os.path.join(os.path.dirname(__file__), "train")  # Train image directory
+train_images_dir = os.path.join(os.path.dirname(__file__), "train_augmented")  # Train image directory
 test_images_dir = os.path.join(os.path.dirname(__file__), "test_labeled")  # Test image directory
 image_size = 64  # The size of the preprocessed image
 nr_bins = 20  # Number of bins in the hue color histogram
@@ -49,8 +70,8 @@ feature_extractors = [hog_extractor]#, symbol_extractor, sift_extractor, shape_e
 tsr = TrafficSignRecognizer()
 
 #tsr.make_submission(train_images_path=train_images_dir, test_images_path=test_images_dir, output_file_path="test.xlsx", feature_extractors=feature_extractors, times=2, size=64)
-#tsr.make_submission(train_images_path=train_images_dir, test_images_path=test_images_dir, output_file_path="test.xlsx", feature_extractors=feature_extractors, times=4, size=64)
+tsr.make_submission(train_images_path=train_images_dir, test_images_path=test_images_dir, output_file_path="test.xlsx", feature_extractors=feature_extractors, times=1, size=64)
 
 
-print(tsr.local_test(train_images_path=train_images_dir, feature_extractors=feature_extractors,k=2, nr_data_augments=1, size=64, times=1))
+#print(tsr.local_test(train_images_path=train_images_dir, feature_extractors=feature_extractors,k=2, nr_data_augments=1, size=64, times=1))
 
